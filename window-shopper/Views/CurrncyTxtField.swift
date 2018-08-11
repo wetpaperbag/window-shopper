6bd7501b6038735c954b75b7766376f4b6d39d8b
Junior <Junior@Family-MacBook.local>
2018-08-11 10:24:11 -0700

    made Views group, edted main storyboard, added text box

---
+++ window-shopper/Views/CurrncyTxtField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrncyTxtField.swift
+//  window-shopper
+//
+//  Created by Junior on 8/11/18.
+//  Copyright © 2018 Junior. All rights reserved.
+//
+
+import UIKit
+
+class CurrncyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2470015405)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
+        }
+        
+       
+    }
+}
